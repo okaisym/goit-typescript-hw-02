c2f9a7678dc4caad32a9b6332a3edf274b2861c6
okaisym <okaisim@gmail.com>
2024-01-02 23:27:21 +0200

    task folder 2 done :)

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface Main {
+  title: string;
+}
 
+class Component<T>{
+  constructor (public props: T) {
   }
 }
 
-class Page extends Component {
+class Page extends Component<Main> {
   pageInfo () {
     console.log(this.props.title);
   }
